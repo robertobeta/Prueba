423bec755d31b354e878e87395f5310f2e77dc31
Roberto Betancourt <145379882+robertobeta@users.noreply.github.com>
2026-07-28 16:50:30 -0600

    Update WeatherForecastController.cs

--- Prueba/Controllers/WeatherForecastController.cs
+++ Prueba/Controllers/WeatherForecastController.cs
@@ -1,21 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Prueba.Models;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Prueba.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class ApiPrueba : ControllerBase
     {
         private readonly VueDbContext _context;
-
-        public WeatherForecastController(VueDbContext context)
+        public class Mensaje
+        {
+            public string? Texto { get; set; }
+        }
+        public ApiPrueba(VueDbContext context)
         {
             _context = context;
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Formulario>>> GetFormulario() => Ok(await _context.Formularios.ToListAsync());
+        public async Task<ActionResult<IEnumerable<Mensaje>>> GetFormulario() =>
+            Ok(new Mensaje { 
+                Texto = "Hola esto es una prueba"
+             });
     }
 }
